d1a1bbf09de179b4f86adf7ca23b96766edc7004
Ayyan <m.ayyan.j29@gmail.com>
2025-10-24 10:46:54 -0700

    Updated recipe view and fixed API key issue

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '915c232c-3af6-4186-8f6d-7d27a43d5073';
 export const MODAL_CLOSE_SEC = 2.5;
